c068b42efe067e06fb80360f0af064019ccf3388
guodayang007 <2757748253@qq.com>
2021-12-22 18:59:16 +0800

    update App.vue update index.vue

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,28 @@
     <div>
       <router-view/>
     </div>
-    <div :style="style.navigation" style="position: fixed;bottom: 0;width: 100%;height: 70px;color: #474747;padding-top: 10px;border-top: 1px solid #eee;background-color: #fff;">
-      <div style="width: 25%;float: left;cursor: pointer; text-align: center">
-        <div :style="style.button" @click="this.$router.replace('/')">
-          首页
-        </div>
+    <div :style="style.navigation"
+         style="position: fixed;bottom: 0;width: 100%;height: 70px;color: #474747;padding-top: 10px;background-color: black;">
+
+      <div :style="style.button" @click="goToHtml('/')" style="text-align: center">
+        <img id="index_img" src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/index_icon.png"
+             style="height: 32px;width: 32px">
+        <div id="index_text" style="font-size: 10px;color: #BEBEBE">首页</div>
       </div>
-      <div style="width: 25%;float: left;cursor: pointer; text-align: center" >
-        <div :style="style.button" @click="this.$router.replace('broadcast')">
-          直播
-        </div>
+      <div :style="style.button" @click="goToHtml('broadcast')" style="text-align: center">
+        <img id="broadcast_img" src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/broadcast_icon.png"
+             style="height: 32px;width: 32px">
+        <div id="broadcast_text" style="font-size: 10px;color: #BEBEBE">直播</div>
       </div>
-      <div style="width: 25%;float: left;cursor: pointer; text-align: center" >
-        <div :style="style.button" @click="this.$router.replace('shopping')">
-          购物车
-        </div>
+      <div :style="style.button" @click="goToHtml('shopping')" style="text-align: center">
+        <img id="shopping_img" src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/shopping_icon.png"
+             style="height: 32px;width: 32px">
+        <div id="shopping_text" style="font-size: 10px;color: #BEBEBE">购物车</div>
       </div>
-      <div style="width: 25%;float: left;cursor: pointer; text-align: center"  >
-        <div :style="style.button" @click="this.$router.replace('shoes')">
-          我的
-        </div>
+      <div :style="style.button" @click="goToHtml('shoes')" style="text-align: center">
+        <img id="my_img" src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/my_icon.png"
+             style="height: 32px;width: 32px">
+        <div id="my_text" style="font-size: 10px;color: #BEBEBE">我的</div>
       </div>
     </div>
   </div>
@@ -30,21 +32,23 @@
 </template>
 
 
-
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 const style = {
   button: {
-    padding: '10px 10px',
-    border: '1px solid #333333',
-    margin: '0px 10px'
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    // padding: '10px 10px',
+    // border: '1px solid #333333',
+    // margin: '0px 10px'
   },
   // changeRouter: {
   //   display: 'flex',
   //   marginTop: '20px',
   // },
-  navigation:{
+  navigation: {
     justifyContent: 'space-evenly',
     alignItems: 'center',
     display: 'flex',
@@ -61,18 +65,52 @@ export default {
       style,
     }
   },
+  mounted: function () {
+    console.log("this.route.path");
+    //broadcast_img
+    //broadcast_text
+    // switch (this.route.path){
+    //   case "/":
+    //   case "/broadcast":
+    //   case "/my":
+    //   case "/shopping":
+    // }
+  },
   methods: {
     //跳转
-    goToHtml () {
-      // var height = $(window).height() - $("header").height() - $("#description").height() - $("#menu").height();
-      // $("#content").height(height + "px");
+    goToHtml(str) {
+      document.getElementById("index_text").style.color = "#BEBEBE"
+      document.getElementById("broadcast_text").style.color = "#BEBEBE"
+      document.getElementById("shopping_text").style.color = "#BEBEBE"
+      document.getElementById("my_text").style.color = "#BEBEBE"
+      document.getElementById("index_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/index_icon.png"
+      document.getElementById("broadcast_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/broadcast_icon.png"
+      document.getElementById("shopping_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/shopping_icon.png"
+      document.getElementById("my_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/my_icon.png"
+      switch (str) {
+        case "/":
+          document.getElementById("index_text").style.color = "#5EDE99"
+          document.getElementById("index_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/index_icon_green.png"
+          // this.$router.replace('/');
+          break;
+        case "broadcast":
+          document.getElementById("broadcast_text").style.color = "#5EDE99"
+          document.getElementById("broadcast_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/broadcast_icon_green.png"
+          // this.$router.replace('broadcast');
+          break;
+        case "shopping":
+          document.getElementById("shopping_text").style.color = "#5EDE99"
+          document.getElementById("shopping_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/shopping_icon_green.png"
+          // this.$router.replace('/');
+          break;
+        case "my":
+          document.getElementById("my_text").style.color = "#5EDE99"
+          document.getElementById("my_img").src = "https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/my_icon_green.png"
+          // this.$router.replace('/');
+          break;
+      }
+      this.$router.replace(str);
     },
-    // goToVue () {
-    //   this.$router.replace('vue')
-    // },
-    // goToKraken () {
-    //   this.$router.replace('kraken')
-    // }
   }
 }
 </script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -16,7 +16,7 @@
 
 <!--    <div style="background-image: url('https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/red_shoes.png');background-repeat: no-repeat;background-position: center 0;background-size: cover;height: 550px;max-width: 550px">-->
     <div style="padding: 20px 20px 0px 20px">
-      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/red_shoes.png" style="width: 100%;height: auto;">
+      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/red_shoes_v1.png" style="width: 100%;height: auto;">
       <div style="height: 100px;margin-top: -100px;padding-left: 20px">
         <div style="font-style: normal;font-weight: bold;font-size: 20px;">Welcome X-S-Store</div>
         <div style="height: 10px"></div>
@@ -25,7 +25,7 @@
     </div>
 
     <div style="padding: 20px 20px 0px 20px">
-      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/white_shoes.png" style="width: 100%;height: auto;">
+      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/white_shoes_v1.png" style="width: 100%;height: auto;">
       <div style="height: 130px;margin-top: -130px;padding-left: 20px">
         <div style="font-style: normal;font-weight: bold;font-size: 20px;">Nike Adapt BB Unboxing - Futuristic Self Lacing Sneakers</div>
         <div style="height: 10px"></div>
@@ -34,7 +34,7 @@
     </div>
 
     <div style="padding: 20px 20px 0px 20px">
-      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/powder_shoes.png" style="width: 100%;height: auto;">
+      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/powder_shoes_v1.png" style="width: 100%;height: auto;">
       <div style="height: 150px;margin-top: -150px;padding-left: 20px">
         <div style="font-style: normal;font-weight: bold;font-size: 20px;">Nike Alphafly Next% Full & Final Review | Carbon Fiber Plate Marathon Shoe</div>
         <div style="height: 10px"></div>
@@ -43,7 +43,7 @@
     </div>
 
     <div style="padding: 20px 20px 0px 20px">
-      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/red_aj_shoes.png" style="width: 100%;height: auto;">
+      <img src="https://miniapp-nikestore-demo.oss-cn-beijing.aliyuncs.com/red_aj_shoes_v1.png" style="width: 100%;height: auto;">
       <div style="height: 100px;margin-top: -100px;padding-left: 20px">
         <div style="font-style: normal;font-weight: bold;font-size: 20px;">About</div>
         <div style="font-style: normal;font-weight: bold;font-size: 20px;">Jordan 1 Mid Chicago Toe</div>

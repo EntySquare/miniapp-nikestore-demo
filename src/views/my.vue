c2c1470b1a40226b6ee7f6bee94911f2983a91e8
guodayang007 <2757748253@qq.com>
2021-12-27 16:30:48 +0800

    update my.vue

--- src/views/my.vue
+++ src/views/my.vue
@@ -1,7 +1,13 @@
 <template>
   <div style="background: black;height: 100vh;width: 100%;text-align: center">
     <div style="height: 50vh"></div>
-    <div style="color: #BEBEBE;font-weight: 900;font-size: 20px">无数据</div>
+    <div style="color: #BEBEBE;font-weight: 900;font-size: 20px">token:</div>
+    <div style="color: #BEBEBE;font-weight: 900;font-size: 20px">{{ tokenStr }}</div>
+    <div style="height: 20px"></div>
+    <button style="background: #5EDE99;color: white" @click="getToken()">get token</button>
+    <div style="height: 20px"></div>
+    <button style="background: #5EDE99;color: white" @click="deleteToken()">delete token</button>
+<!--    <div :style="style.button" @click="goToHtml('broadcast')" style="text-align: center">get token</div>-->
   </div>
 </template>
 
@@ -19,7 +25,24 @@ export default {
   data() {
     return {
       style,
+      tokenStr: "null",
     }
   },
+  methods: {
+    //跳转
+    getToken() {
+      // eslint-disable-next-line no-undef
+      kraken.methodChannel.invokeMethod('getToken')
+          .then(result => {
+            this.tokenStr = result.toString();
+          })
+          .catch(err => {
+            this.tokenStr = err.toString();
+          });
+    },
+    deleteToken() {
+      this.tokenStr = "null"
+    }
+  }
 }
 </script>
